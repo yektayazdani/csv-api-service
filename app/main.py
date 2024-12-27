90102c4efbd2a59aa2716b67d80fc6efff023872
Yekta Yazdani <y_1ta@yahoo.com>
2024-12-27 13:21:32 -0800

    added the csv read service

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from app.csv_reader import read_csv_file
+#from app.api_client import make_api_call
+import logging
+from app.utils import setup_logger
+
+logger = setup_logger(__name__)
+
+def main():
+    """Main function to read CSV and make API calls."""
+    items = read_csv_file()
+    if not items:
+         logger.error("No items to process.")
+         return
+    # for item in items:
+    #    api_response = make_api_call(item.data)
+    #    if api_response:
+    #        logger.info(f"Successfully processed: {item.data}, response {api_response}")
+
+
+if __name__ == "__main__":
+    main()
