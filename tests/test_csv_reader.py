b05939e28ca1f879fbaa82fe3ea734873abd6a23
yekta yazdani <yektayazdani@yektas-MacBook-Pro.local>
2024-12-30 18:13:24 -0800

    updated the way reader gets the location

--- tests/test_csv_reader.py
+++ tests/test_csv_reader.py
@@ -1,13 +1,12 @@
 import unittest
 import os
 from app.csv_reader import read_csv_file
-from app.config import CSV_FILE_PATH
 
 class TestCsvReader(unittest.TestCase):
     
     def setUp(self):
         # Create a dummy CSV file for testing
-        self.test_csv_path = "data/input2.csv" #CSV_FILE_PATH
+        self.test_csv_path = "data/input2.csv" 
         with open(self.test_csv_path, 'w') as f:
             f.write("col1,col2\n")
             f.write("val1,val2\n")
@@ -19,18 +18,15 @@ class TestCsvReader(unittest.TestCase):
         os.remove(self.test_csv_path)
         
     def test_read_csv_file(self):
-        items = read_csv_file()
+        items = read_csv_file(self.test_csv_path)
         self.assertEqual(len(items), 2)
         self.assertEqual(items[0].data['col1'], 'val1')
         self.assertEqual(items[1].data['col2'], 'val4')
         
     def test_read_csv_file_not_found(self):
         #Change the file path temporarily so it raises file not found exception
-        old_path = CSV_FILE_PATH
-        os.environ["CSV_FILE_PATH"] = "non_existent_file.csv"
-        items = read_csv_file()
+        items = read_csv_file("non_existent_file.csv")
         self.assertEqual(len(items), 0)
-        os.environ["CSV_FILE_PATH"] = old_path
 
 if __name__ == '__main__':
     unittest.main()
